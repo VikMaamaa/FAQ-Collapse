f11064c236a1dfa9b07f5cb756e0f7344f31f243
VikMaamaa <73889664+VikMaamaa@users.noreply.github.com>
2021-08-05 03:04:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/*eslint-disable*/
+
+const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
